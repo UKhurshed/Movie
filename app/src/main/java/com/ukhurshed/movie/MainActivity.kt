4039627ccd25199521ca2214ec22289490e9e01e
khurshed <khurshedumarov777@gmail.com>
2020-04-19 13:10:03 +0500

    First fragment page

--- app/src/main/java/com/ukhurshed/movie/MainActivity.kt
+++ app/src/main/java/com/ukhurshed/movie/MainActivity.kt
@@ -9,15 +9,18 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var click_trailer: TextView
+    private val fragmnentManager = supportFragmentManager
+    private val mainFragment = MainFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        click_trailer = findViewById(R.id.click_to_trailer)
-        click_trailer.setOnClickListener(){
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=TcMBFSGVi1c")))
-        }
+        val fragmentManager = fragmnentManager.beginTransaction()
+            .add(R.id.gods_fragment_details, mainFragment)
+            .commit()
+
+
+
     }
 }
