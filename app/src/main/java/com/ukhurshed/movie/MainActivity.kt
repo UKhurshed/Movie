f34ce637723b26a96f4ce1b135f8a163edc3ae5c
khurshed <khurshedumarov777@gmail.com>
2020-05-01 13:53:05 +0500

    Upgrade Project

--- app/src/main/java/com/ukhurshed/movie/MainActivity.kt
+++ app/src/main/java/com/ukhurshed/movie/MainActivity.kt
@@ -9,15 +9,9 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var click_trailer: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        click_trailer = findViewById(R.id.click_to_trailer)
-        click_trailer.setOnClickListener(){
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=TcMBFSGVi1c")))
-        }
     }
 }
