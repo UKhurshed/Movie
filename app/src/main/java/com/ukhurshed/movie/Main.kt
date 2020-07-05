76a277763266eeac560ecfe2537bf5aa1cb037d2
khurshed <khurshedumarov777@gmail.com>
2020-07-05 22:41:07 +0500

    Update environment

--- app/src/main/java/com/ukhurshed/movie/Main.kt
+++ app/src/main/java/com/ukhurshed/movie/Main.kt
@@ -1,17 +1,5 @@
 package com.ukhurshed.movie
 
 fun main(){
-    val cat = Animal("Musya")
-    cat.age = 7
-    val dog = Animal("Lusya")
-    print(dog.age)
-    print(dog.age - cat.age)
-}
 
-fun multiple(first: Int = 0, second: Int){
-    print("Result: ${first*second}")
-}
-
-class Animal(val name: String) {
-    var age: Int = 6
 }
